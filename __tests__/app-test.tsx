a651790ae2ce351eceaf80f2388b3304591ee848
Helder Pinhal <helder@notifica.re>
2020-04-29 15:05:47 +0100

    Boilerplate project

---
+++ __tests__/app-test.tsx
@@ -0,0 +1,12 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer'; // Note: test renderer must be required after react-native.
+import App from '../src/app';
+
+it('renders correctly', () => {
+  renderer.create(<App />);
+});
