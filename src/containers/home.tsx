af192278eb69c931a26fcd08f250e1dfb718d0ba
Helder Pinhal <helder@notifica.re>
2020-05-06 12:11:48 +0100

    Listen for deep links in the home page

--- src/containers/home.tsx
+++ src/containers/home.tsx
@@ -1,18 +1,33 @@
-import React, { FC } from 'react';
-import { StyleSheet } from 'react-native';
+import React, { FC, useEffect, useRef } from 'react';
+import { Linking, StyleSheet } from 'react-native';
 import WebView from 'react-native-webview';
 import { useNetworkRequest } from '../lib/machines/network';
 import { getDemoSourceConfig } from '../lib/utils/storage';
 import { Loader } from '../components/loader';
+import { WebViewNavigation } from 'react-native-webview/lib/WebViewTypes';
+import { useNavigation } from '@react-navigation/native';
 
 export const Home: FC = () => {
   const [state] = useNetworkRequest(getDemoSourceConfig(), { autoStart: true });
+  useDeepLinking();
+
+  const handleNavigationRequest = (event: WebViewNavigation) => {
+    console.log('handleNavigationRequest');
+    console.log(event);
+    return true;
+  };
 
   return (
     <>
       {(state.status == 'idle' || state.status == 'pending') && <Loader />}
 
-      {state.status == 'successful' && <WebView source={{ uri: state.result!.url }} />}
+      {state.status == 'successful' && (
+        <WebView
+          source={{ uri: state.result!.url }}
+          javaScriptEnabled={true}
+          onShouldStartLoadWithRequest={(event) => handleNavigationRequest(event)}
+        />
+      )}
     </>
   );
 };
@@ -24,3 +39,27 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const useDeepLinking = () => {
+  const navigation = useNavigation();
+
+  const handleDeepLinkingUrl = (url: string | null) => {
+    console.log(`Received a deep link: ${url}`);
+  };
+
+  // Removing a listener requires a reference to the original listener.
+  const handleDeepLinkingEvent = (event: { url: string }) => handleDeepLinkingUrl(event.url);
+
+  useEffect(() => {
+    // Check if the app was started with a deep link.
+    Linking.getInitialURL()
+      .then((url) => handleDeepLinkingUrl(url))
+      .catch((e) => console.log(`Failed to get the initial deep link: ${e}`));
+
+    // Listen for deep linking event.
+    Linking.addEventListener('url', handleDeepLinkingEvent);
+
+    // Clean up the listener.
+    return () => Linking.removeEventListener('url', handleDeepLinkingEvent);
+  }, []);
+};
